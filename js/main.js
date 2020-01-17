84c786cbe2f75bbb085d66ff3d502125a6599429
Nguyen Cong DOan <doannc2212@gmail.com>
2020-01-18 01:14:58 +0700

    update "blog"

--- js/main.js
+++ js/main.js
@@ -58,3 +58,11 @@ function showQuestionText(index) {
     }
   }
 }
+
+
+wow = new WOW({
+  animateClass: 'animated',
+  offset: 100,
+  mobile: false, // trigger animations on mobile devices (default is true)
+});
+wow.init();
